76c9cffc9dc20f6a0135e5efe48eaad570cd7388
Alex <huvboo@126.com>
2022-02-15 18:04:24 +0800

    Update cwc-button.js

--- src/ui/cwc-button.js
+++ src/ui/cwc-button.js
@@ -1,10 +1,31 @@
 import CustomWebComponent from '../CustomWebComponent.js'
 
 class CwcButtom extends CustomWebComponent {
-  constructor() {
-    super({ props: { label: 'Button' }, template: '<button></button>' })
+  constructor(props = { label: 'Button', backgroundColor, color }) {
+    super({
+      props,
+      template: `
+      <style>
+      :host {
+        display: inline-block;
+        height: 20px;
+        padding: 1px 6px;
+        border-radius: 4px;
+        background-color: #00A1F1;
+        color: #FFF;
+        font-size: 12px;
+        line-height: 1.5;
+        cursor: default;
+        user-select: none;
+      }
+      </style>
+      `,
+    })
 
-    this.internals.shadowRoot.childNodes[0].innerText = this.props.label
+    this.internals.shadowRoot.childNodes[0].nodeValue = this.props.label
+    if (this.props.backgroundColor)
+      this.style.backgroundColor = this.props.backgroundColor
+    if (this.props.color) this.style.color = this.props.color
   }
 }
 
